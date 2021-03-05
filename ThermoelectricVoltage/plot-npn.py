28bb78a2b4f34766d65e6a7a4d00ba8b664c56ad
YangTing <thermoelectrc-cintra@gmail.com>
2021-03-05 10:13:45 +0800

    testing mosfet

--- ThermoelectricVoltage/plot-npn.py
+++ ThermoelectricVoltage/plot-npn.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
 
-datas = pd.read_csv("testing-npn.csv")
+datas = pd.read_csv("testing-MOSFET.csv")
 
-x = datas["Base_Voltage"]
-y = datas["BE_Current"] * 1000
+x = datas["Gate_Voltage"]
+y = datas["Drain_Current"] * 1000
 
 plt.plot(x, y)
-plt.xlabel("Base voltage (V)")
-plt.ylabel("Base Current (mA)")
+plt.xlabel("Gate voltage (V)")
+plt.ylabel("Drain Current (mA)")
 plt.show()
--- ThermoelectricVoltage/test-mosfet.py
+++ ThermoelectricVoltage/test-mosfet.py
@@ -8,26 +8,26 @@ vce = Keithley2420("GPIB0::6")
 gatevoltage.measure_current()
 vce.measure_current()
 voltage = 0
-while voltage <= 5:
+while voltage <= 150:
     gatevoltage.source_voltage = voltage
-    vce.source_voltage = 2
-    fileName = "testing-npn.csv"
+    vce.source_voltage = 0.5
+    fileName = "testing-MOSFET.csv"
     with open(fileName, 'a', newline='') as csvfile:
-        header = ["Base_Voltage", "BE_Current","CE_Voltage", "CE_Current"]
+        header = ["Gate_Voltage", "Gate_Current","Drain_Voltage", "Drain_Current"]
         writer = csv.DictWriter(csvfile, fieldnames=header)
         if csvfile.tell() == 0:
             writer.writeheader()
         writer.writerow(
             {
-                "Base_Voltage": voltage,
-                "BE_Current": gatevoltage.current,
-                "CE_Voltage": vce.source_voltage,
-                "CE_Current": vce.current
+                "Gate_Voltage": voltage,
+                "Gate_Current": gatevoltage.current,
+                "Drain_Voltage": vce.source_voltage,
+                "Drain_Current": vce.current
             }
         )
     csvfile.close()
     time.sleep(1)
-    voltage += 0.1
+    voltage += 1
 
 # voltage = 0
 # while voltage <= 5:
